b14a11ba56b8860c61ca0bb654f842ee5576d905
AsadMondallg <asad428161@gmail.com>
2023-09-28 16:31:32 +0600

    first commit

---
+++ config/config.php
@@ -0,0 +1,7 @@
+<?php
+define("DB_HOST", "sql302.infinityfree.com");
+define("DB_USER", "if0_35125243");
+define("DB_PASS", "Sj7NeVTZSCOQ2pF");
+define("DB_NAME", "if0_35125243_personal_blog");
+define("TITLE", "Personal blog of Asad Mondall");
+define("KEYWORDS", "PROGRAMMING, DATA SCIENCE, DEEP LEARNING");
